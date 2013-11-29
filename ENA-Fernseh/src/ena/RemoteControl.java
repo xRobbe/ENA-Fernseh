c6afa6d846374313cc0e36768a3de7d27976f3f7
xRobbe <Robin.Rodenhausen@gmail.com>
2013-11-29 21:58:48 +0100

    Create Classes

---
+++ ENA-Fernseh/src/ena/RemoteControl.java
@@ -0,0 +1,43 @@
+package ena;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class RemoteControl {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RemoteControl window = new RemoteControl();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RemoteControl() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
